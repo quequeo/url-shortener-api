1dd3958cb83f14f153d07c1b6cbcef01a966772b
Jaime García Méndez <jaimegarciamendez@gmail.com>
2026-02-06 19:48:41 -0500

    Cors, Session/Cookie middleware

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
   include Errorable
 
   respond_to :json
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,21 @@
+module Users
+  class SessionsController < Devise::SessionsController
+    respond_to :json
+
+    private
+
+    def respond_with(resource, _opts = {})
+      render json: {
+        user: {
+          id: resource.id,
+          email: resource.email,
+          name: resource.name
+        }
+      }, status: :ok
+    end
+
+    def respond_to_on_destroy
+      head :no_content
+    end
+  end
+end
